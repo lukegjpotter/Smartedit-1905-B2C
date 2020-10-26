2bc16665916345c9aed580dd74de2d2d8b0a6284
Matysik <lukasz.matysik@sap.com>
2020-10-26 13:03:19 +0100

    adding ext

---
+++ core-customize/npm-ancillary/npmancillary/gensrc/de/hybris/constants/GeneratedNpmancillaryConstants.java
@@ -0,0 +1,31 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Oct 21, 2020, 1:25:30 PM                    ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedNpmancillaryConstants
+{
+	public static final String EXTENSIONNAME = "npmancillary";
+	
+	protected GeneratedNpmancillaryConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
